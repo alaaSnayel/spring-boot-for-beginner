e7648de4c15fbc9a6ac272a21ee34cc6678452ae
alaaSnayel <alaanayel111@gmail.com>
2025-05-19 14:54:09 +0300

    implementing dao layer

--- src/main/java/com/alaa/springdemo/student/StudentController.java
+++ src/main/java/com/alaa/springdemo/student/StudentController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/v1/students")
@@ -16,8 +21,18 @@ public class StudentController {
   
   // Constructor injection
   private final StudentService service;
-  public StudentController(StudentService studentService) {
-    this.service = studentService;
+  public StudentController(StudentService service) {
+    this.service = service;
+  }
+  
+  @PostMapping
+  public Student save(@RequestBody Student student) {
+    return service.save(student);
+  }
+
+  @GetMapping("/{email}")
+  public Student findByEmail(@PathVariable("email") String email) {
+    return service.findByEmail(email);
   }
 
   @GetMapping
@@ -25,4 +40,13 @@ public class StudentController {
     return service.getAllStudents();
   }
 
+  @PutMapping
+  public Student updateStudent(@RequestBody Student student) {
+    return service.update(student);
+  }
+
+  @DeleteMapping("/{email}")
+  public void deleteStudent(@PathVariable("email") String email) {
+    service.delete(email);
+  }
 }
--- src/main/java/com/alaa/springdemo/student/StudentService.java
+++ src/main/java/com/alaa/springdemo/student/StudentService.java
@@ -1,37 +1,16 @@
 package com.alaa.springdemo.student;
 
-import java.time.LocalDate;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
+public interface StudentService {
 
-@Service
-public class StudentService {
+  Student save(Student s);
+  
+  List<Student> getAllStudents();
+  
+  Student findByEmail(String email);
 
-  public List<Student> getAllStudents() {
-    return List.of(
-      new Student(
-        "Alaa",
-        "Sayed",
-        LocalDate.of(1998, 10, 1),
-        "alaanayel111@gmail.com",
-        26
-        ),
-      new Student(
-        "Diaa",
-        "Nayel",
-        LocalDate.of(1997, 2, 12),
-        "diaanayel@gmail.com",
-        28
-      ),
-      new Student(
-        "Wael",
-        "Hesham",
-        LocalDate.of(1993, 5, 10),
-        "waelh298@gmail.com",
-        32
-      )
-    );
-  }
+  Student update(Student s);
 
+  void delete(String email);
 }
